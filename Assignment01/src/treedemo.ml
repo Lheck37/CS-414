297bd110c1c26c190ef777dde9dca31b4cb52cac
Lheck37 <lheck@my.athens.edu>
2025-09-01 16:32:36 -0500

    Update treedemo.ml

--- Assignment01/src/treedemo.ml
+++ Assignment01/src/treedemo.ml
@@ -6,18 +6,22 @@ let a_bintree =
     Bintree.Tree.Empty
     values
 
+(* helper to print list-of-lists like [[10]; [5; 15]; [3; 7]] *)
+let show_levels lvls =
+  lvls
+  |> List.map (fun lvl ->
+       "[" ^ (lvl |> List.map string_of_int |> String.concat "; ") ^ "]")
+  |> String.concat "; "
+
 let () =
   (* print the whole tree *)
   let s = Bintree.Tree.string_of_tree a_bintree in
   print_endline s;
 
   (* show Assignment 1 required functions *)
-  print_endline "Level-order:";
-  Bintree.Tree.level_traversal a_bintree
-  |> List.map string_of_int
-  |> String.concat " "
-  |> print_endline;
+  print_endline "Level-by-level:";
+  let levels = Bintree.Tree.level_traversal a_bintree in
+  print_endline ("[" ^ show_levels levels ^ "]");
 
   print_endline "Pruned:";
   print_endline (Bintree.Tree.string_of_tree (Bintree.Tree.prune a_bintree))
-
