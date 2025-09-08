c78a8532883b96f06ec431512e0d435a699f4e57
Lheck37 <lheck@my.athens.edu>
2025-09-08 08:40:17 -0500

    Update regex_tests.py

--- Assignment02/regex_tests.py
+++ Assignment02/regex_tests.py
@@ -5,8 +5,8 @@ pattern_id = r'^[A-Za-z_][A-Za-z0-9_]*$'
 for t in ["var1", "_thing", "9bad"]:
     print("ID:", t, "->", bool(re.match(pattern_id, t)))
 
-# 2) U.S. phone numbers
-pattern_phone = r'^(\(\d{3}\)\s|\d{3}-)\d{3}-\d{4}$'
+# 2) U.S. phone numbers: (XXX) XXX-XXXX or XXX-XXX-XXXX
+pattern_phone = r'^(\(\d{3}\) \d{3}-\d{4}|\d{3}-\d{3}-\d{4})$'
 for t in ["(256) 555-1212", "256-555-1212", "5551212"]:
     print("Phone:", t, "->", bool(re.match(pattern_phone, t)))
 
@@ -19,4 +19,3 @@ for t in ["3.14", "-0.5", "+42", ".", "abc"]:
 pattern_binpal = r'^(000|010|101|111|0110|1001|1111|0000)$'
 for t in ["101", "0110", "111", "0101"]:
     print("BinPal:", t, "->", bool(re.match(pattern_binpal, t)))
-
